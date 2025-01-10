4d7ffe6924b97ea6f1b4c4625b7ffc7a46e31d95
VanHungNguyen <hung-nguyen@anrakutei.vn>
2025-01-10 11:37:40 +0700

    .

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import TagManager from 'react-gtm-module'
 import App from './App'
 import './index.css'
+
+const tagManagerArgs = {
+	gtmId: 'GTM-PRZNM8FL', // Thay bằng mã GTM của bạn
+}
+
+TagManager.initialize(tagManagerArgs)
 
 ReactDOM.render(
 	<React.StrictMode>
